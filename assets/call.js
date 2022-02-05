920710223f8af1905a92198cc3c041ea01e3c28f
BinaryBitBytes <94703967+BinaryBitBytes@users.noreply.github.com>
2022-02-05 01:19:49 -0500

    started outlining the script for the cities  and functions

---
+++ assets/call.js
@@ -0,0 +1,20 @@
+var container= document.getElementById('');
+var searchButton = document.getElementById('search-button');
+
+function getApi(){ //
+    var ROOTURLloc = "http://api.openweathermap.org/geo/1.0/direct?"; //
+    
+    fetch(ROOTURLloc) //
+    .then (function (load){ //
+        console.log(load); //
+        for (var i=0; i<load.length; i++){ //
+            var city= document.createElement()//
+            var state= document.createElement()//
+            city.textContent = load[i].city.state;
+            state.textContent= load[i].name;
+            container.append(city);
+            container.append(state);
+        }
+    });
+}
+searchButton.addEventListener('click', getApi);
---
+++ assets/script.js
@@ -0,0 +1,13 @@
+//location for query
+var ROOTURLloc= "http://api.openweathermap.org/geo/1.0/direct?"
+//syntax/ q={city name},{state code},{country code}&limit={limit}&appid={API key}
+var city=;
+var cityName= q=city,state"&limit=5&appid={API KEY}
+
+// Data for query
+var ROOTURLdata= 'https://api.openweathermap.org/data/2.5/onecall?';
+var searchUrl =(ROOTURL+searchCityUrl+uvIndexUrl);
+var searchCityUrl =;
+var uvIndexUrl = '&daily.uvi';
+var view5dayUrl='&';
+var currentConditionsUrl=current ;
