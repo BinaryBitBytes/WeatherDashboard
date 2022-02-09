01bd44532ba741923090db799bbb88eb8fbdc251
BinaryBitBytes <94703967+BinaryBitBytes@users.noreply.github.com>
2022-02-08 19:47:01 -0500

    updated previous local repo push to main

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@
 var ROOTURLloc= "http://api.openweathermap.org/geo/1.0/direct?"
 //syntax/ q={city name},{state code},{country code}&limit={limit}&appid={API key}
 var city=;
-var cityName= q=city,state"&limit=5&appid={API KEY}
+var cityName= q=city+state+"&limit=5&appid={API KEY}
 
 // Data for query
 var ROOTURLdata= 'https://api.openweathermap.org/data/2.5/onecall?';
