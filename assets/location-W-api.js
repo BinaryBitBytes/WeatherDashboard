4aba81a0ea66a32562dd4b96cf3b25c06719d92f
BinaryBitBytes <94703967+BinaryBitBytes@users.noreply.github.com>
2022-02-09 14:37:59 -0500

    to get the mvp i spent a moment to use comments to isolate the calls in the weather api for the content that is needed.

---
+++ assets/location-W-api.js
@@ -0,0 +1,49 @@
+//api key openweathers 5fbe45de6f75b2400e38d4b9d10635a3
+/*Current Weather Geocoding API //REVERSE GEOCODING
+//https://openweathermap.org/api/geocoding-api#reverse // resource
+http://api.openweathermap.org/geo/1.0/reverse?lat={lat}&lon={lon}&limit={limit}&appid={API key}
+*/
+
+/*coord
+coord.lon City geo location, longitude
+coord.lat City geo location, latitude*/
+
+/**WHEN I view current weather conditions for that city 
+**THEN I am presented with the city name, the date, an icon representation of weather conditions,
+*!the temperature, the humidity, the wind speed, and the UV index
+*?"name": "City of London",   //<------ MVP item CITY NAME
+* TODO: "lat": 51.5128,
+*@param thisParam"lon": -0.0918,
+/////USE ONE-CALL-API// SOURCE///https://openweathermap.org/api/one-call-api
+"current": {
+    "dt": 1618317040,   //<---Times of data receive in UNIX GMT time. find a way to convert unix time stamp 
+    "temp": 284.07,     //<----- Temp needs to be converted to imperial (kelvin to Farenheit)
+    "humidity": 62,  //<------ MVP item HUMIDITY
+    "uvi": 0.89,     //<------ MVP item UV index
+    "wind_speed": 6,  //<------ MVP item WIND_SPEED
+    "weather": [ ** More Weather Data //<--- icon weather conditions ~~
+~~~~~~~~~~~~~
+current.rain (where available) Precipitation volume, mm
+current.snow (where available) Snow volume, mm
+current.weather
+current.weather.id Weather condition id
+current.weather.main Group of weather parameters (Rain, Snow, Extreme etc.)
+current.weather.description Weather condition within the group (full list of weather conditions). Get the output in your language
+current.weather.icon Weather icon id. How to get icons
+~~~~~~~~~~~~~~
+/*Current Weather Data
+api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+*/
+
+/*
+? resource link https://openweathermap.org/forecast5
+*FUTURE WEATHER DATA
+!5-day forecast::[date]::[WeatherIcon]::[Temperature]::[WindSpeed]::Humidty
+* TODO: api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+!5-day-forecast: catogory
+!list.dt <--- Time date List
+!list.weather.icon <--- Weather Icon
+!list.main.temp <---- Temperature !! NEEDS TO BE IMPERIAL!
+!list.wind.speed <---- Wind Speed !! NEEDS TO BE IMPERIAL!
+!list.main.humidity
+*/
--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,18 @@
+//my api key = 5fbe45de6f75b2400e38d4b9d10635a3
 //location for query
+var requestUrl="http://api.openweathermap.org/geo/1.0/direct?&appid=5fbe45de6f75b2400e38d4b9d10635a3"  // declaring the var  to request the URL needed for the fetch. WRAP URL in QUOTES
+fetch(requestUrl)
+.then(function(response){ //.then means next step
+    return response.json(); //.json () method is .http and we need to extract json from response
+}) // usong fetch to do fetch request to server and then parse http respnse into json
+.then(function(data){
+    console.log(data); //returns the data from our fetch(response) and puts data in console.
+})
+//units=imperial //For temperature in Fahrenheit and wind speed in miles/hour
+//example api.openweathermap.org/data/2.5/onecall?lat=30.489772&lon=-99.771335&units=imperial
+
+
+/*
 var ROOTURLloc= "http://api.openweathermap.org/geo/1.0/direct?"
 //syntax/ q={city name},{state code},{country code}&limit={limit}&appid={API key}
 var city=;
@@ -11,3 +25,4 @@ var searchCityUrl =;
 var uvIndexUrl = '&daily.uvi';
 var view5dayUrl='&';
 var currentConditionsUrl=current ;
+*/
